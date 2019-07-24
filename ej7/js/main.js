c5e94c31ebbf375257bbe49cf4395bf90e75f137
Migara <evamarcod@gmail.com>
2019-07-24 11:51:19 +0200

    Change 7 ex with list

--- ej7/js/main.js
+++ ej7/js/main.js
@@ -1,10 +1,25 @@
 'use strict';
 
 const button = document.querySelector('.btn');
+const item1 = document.querySelector('.list_item1');
+const item2 = document.querySelector('.list_item2');
+const item3 = document.querySelector('.list_item3');
 
 function toggle(event){
-    button.classList.toggle('btn1');
+    console.log('Funciono :D');
+    // const selected = event.currentTarget;
+    const selected = event.target;
+    const value = selected.innerHTML;
+    button.classList.remove(`red`);
+    button.classList.remove(`green`);
+    button.classList.remove(`blue`);
+    button.classList.remove(`btn1`);
+    button.classList.add(`${value}`);
     console.log(event);
 };
 
-button.addEventListener('click', toggle);
+window.addEventListener('click', toggle);
+// item1.addEventListener('click', toggle);
+// item2.addEventListener('click', toggle);
+// item3.addEventListener('click', toggle);
+// button.addEventListener('click', toggle);
