ba89226b649d79c4e3a2b5cba61fedbc0ddfc3f4
Migara <evamarcod@gmail.com>
2019-07-24 11:19:47 +0200

    Add new exercise

--- ej7/js/main.js
+++ ej7/js/main.js
@@ -1,10 +1,10 @@
 'use strict';
 
-
-const button= document.querySelector('.btn');
+const button = document.querySelector('.btn');
 
 function toggle(event){
     button.classList.toggle('btn1');
     console.log(event);
 };
+
 button.addEventListener('click', toggle);
